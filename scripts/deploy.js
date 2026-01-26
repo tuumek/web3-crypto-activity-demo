8b625e820e1e314c76d622a35251910a5b244d48
tuumek <136378271+tuumek@users.noreply.github.com>
2026-01-26 22:12:26 +0100

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,11 @@
+const hre = require("hardhat");
+
+async function main() {
+  // Pobranie kontraktu
+  const SimpleStorage = await hre.ethers.getContractFactory("SimpleStorage");
+
+  // Deploy kontraktu
+  const simpleStorage = await SimpleStorage.deploy();
+
+  await simpleS
 
